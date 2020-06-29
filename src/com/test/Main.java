b52708dc9c1c397b6d5eb02285c247875e9ba7db
Zsanett Jakab <zsanett28@yahoo.com>
2020-06-29 15:53:19 +0300

    Zsani commit 2

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Orsi uzen!");
-        System.out.println("Sziasztok!");
+        System.out.println("Sziasztok 1!");
         System.out.println("Zsani uzenete!");
     }
 }
