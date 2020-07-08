0914d3616313619913251112a427251d9a00ce43
Zsanett Jakab <zsanett28@yahoo.com>
2020-07-08 15:15:56 +0300

    JComboBox working with a String array. Created timetable.txt file. Added to the routes.txt file valid data.

--- src/com/Railway/RailwayGUI.java
+++ src/com/Railway/RailwayGUI.java
@@ -2,10 +2,14 @@ package com.Railway;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import java.util.ArrayList;
 
 public class RailwayGUI extends JFrame {
     private JPanel mainPanel;
+    private JComboBox toCity;
     private JComboBox fromCity;
+    String[] string = {"Cluj-Napoca","Târgu-Mureș","Miercurea-Ciuc", "Timișoara", "Oradea", "Craiova",
+            "București", "Iași", "Constanța", "Brașov", "Sibiu", "Suceava", "Târgu-Jiu", "Alba-Iulia"};
 
     public RailwayGUI(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -13,9 +17,12 @@ public class RailwayGUI extends JFrame {
         mainPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
         this.pack();
         this.setTitle("Railway Optimisation System");
+        fromCity.setModel(new DefaultComboBoxModel<String>(string));
+        toCity.setModel(new DefaultComboBoxModel<String>(string));
     }
 
     public static void main(String[] args) {
+
         JFrame frame = new RailwayGUI();
         frame.setSize(800, 600);
         frame.setVisible(true);
