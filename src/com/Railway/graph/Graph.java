69229f009d31a70b8b272f605c652fc1bebaa05e
Zsanett Jakab <zsanett28@yahoo.com>
2020-07-12 17:42:17 +0300

    Graph class, the representation, constructors, methods and the Dijkstra algorithm rewritten

--- src/com/Railway/graph/Graph.java
+++ src/com/Railway/graph/Graph.java
@@ -19,53 +19,64 @@ public class Graph {
     }
 
     public void addEdge(String fromCity, String toCity, double time, double price) {
-        edges.add(new Edge(stringToNode.get(fromCity), stringToNode.get(toCity), time, price));
+        Node fromNode = stringToNode.get(fromCity);
+        Node toNode = stringToNode.get(toCity);
+        Edge edge = new Edge(fromNode, toNode, time, price);
+        edges.add(edge);
+        fromNode.addEdge(edge);
+        toNode.addEdge(edge);
     }
 
     //Dijkstra algorithm
-    public void dijkstra(int fromNode, Weight weight) {
-        //initialization of the nodes
+    public void dijkstra(String source, Weight weight) {
+        dijkstra(stringToNode.get(source), weight);
+    }
+
+    private void dijkstra(Node source, Weight weight) {
+        initializeDijkstra(source);
+
+        Set<Node> visitedNodes = new HashSet<>();
+        Queue<Node> nodeQueue = new PriorityQueue<>(new NodeComparator());
+        nodeQueue.add(source);
+
+        while (!nodeQueue.isEmpty()) {
+            Node minNode = nodeQueue.remove();
+            visitedNodes.add(minNode);
+
+            for (Edge edge : minNode.getEdges()) {
+                Node neighbour = edge.getNeighbour(minNode);
+                double w = weight.getWeight(edge);
+
+                relax(minNode, neighbour, w);
+
+                if (!visitedNodes.contains(neighbour)) {
+                    nodeQueue.add(neighbour);
+                }
+            }
+        }
+    }
+
+    private void relax(Node current, Node neighbour, double weight) {
+        if (neighbour.getDistance() > current.getDistance() + weight) {
+            neighbour.setDistance(current.getDistance() + weight);
+            neighbour.setParent(current);
+        }
+    }
+
+    private void initializeDijkstra(Node source) {
         for (Node node : nodes) {
-            node.setDistanceFromSource(Double.MAX_VALUE);
+            node.setDistance(Double.MAX_VALUE);
             node.setParent(null);
             node.setVisited(false);
         }
-        nodes[fromNode].setDistanceFromSource(0);
-        int nextNode = fromNode;
-
-        for (int i = 0; i < nodes.length; i++) {
-            ArrayList<Edge> currentNodeEdges = nodes[nextNode].getEdges();
-            for (Edge currentNodeEdge : currentNodeEdges) {
-                int neighbourIndex = currentNodeEdge.getNeighbourIndex(nextNode);
-
-                if (!nodes[neighbourIndex].isVisited()) {
-                    double tentative = nodes[nextNode].getDistanceFromSource() + weight.getWeight(currentNodeEdge);
-                    if (tentative < nodes[neighbourIndex].getDistanceFromSource()) {
-                        nodes[neighbourIndex].setDistanceFromSource(tentative);
-                        nodes[neighbourIndex].setParent(nodes[nextNode]);
-                    }
-                }
-            }
-            nodes[nextNode].setVisited(true);
-            nextNode = getNodeShortestDistanced(i);
-        }
-
+        source.setDistance(0);
     }
 
-    private int getNodeShortestDistanced(int j) {
-        int storedNodeIndex = 0;
-        double storedDist = Double.MAX_VALUE;
-        for (int i = 0; i < nodes.length; i++) {
-            double currentDist = nodes[i].getDistanceFromSource();
-            if (!nodes[i].isVisited() && currentDist < storedDist) {
-                storedDist = currentDist;
-                storedNodeIndex = i;
-            }
-        }
-        return storedNodeIndex;
+    public List<String> getPath(String target) {
+        return getPath(stringToNode.get(target));
     }
 
-    public List<String> printPath(Node target) {
+    private List<String> getPath(Node target) {
         ArrayList<String> path = new ArrayList<>();
         for (Node node = target; node != null; node = node.getParent()) {
             path.add(node.getCityName());
@@ -75,26 +86,7 @@ public class Graph {
         return path;
     }
 
-    public void printResult(int fromNode, int toNode) {
-        System.out.println("The shortest distance from " + nodes[fromNode].getCityName() + " to " + nodes[toNode].getCityName() + " is " + nodes[toNode].getDistanceFromSource());
-        System.out.println("The route: " + printPath(nodes[toNode]).toString().replace(","," ->").replace("[","").replace("]",""));
-        System.out.println();
-    }
-
-    //getters
-    public Node[] getNodes() {
-        return nodes;
-    }
-
-    public Edge[] getEdges() {
-        return edges;
-    }
-
-    public int getNoOfNodes() {
-        return noOfNodes;
-    }
-
-    public int getNoOfEdges() {
-        return noOfEdges;
+    public double getDistanceFromSource(String name) {
+        return stringToNode.get(name).getDistance();
     }
 }
