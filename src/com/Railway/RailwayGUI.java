1948126e8f2042e8425b175a36658426af839c51
Zsanett Jakab <zsanett28@yahoo.com>
2020-07-15 23:25:46 +0300

    Methods reorganized

--- src/com/Railway/RailwayGUI.java
+++ src/com/Railway/RailwayGUI.java
@@ -16,7 +16,7 @@ import static com.Railway.Main.printResult;
 public class RailwayGUI extends JFrame {
     private JPanel mainPanel;
     private JComboBox<String> toCity;
-    private JComboBox<String>  fromCity;
+    private JComboBox<String> fromCity;
     private JPanel routeSelectPanel;
     private JPanel buttonPanel;
     private JButton submitButton;
@@ -31,70 +31,70 @@ public class RailwayGUI extends JFrame {
     private JLabel infoLabel;
     private JLabel radioMessageLabel;
     private JLabel comboBoxMessageLabel;
-    private Graph graph;
+    private final Graph graph;
 
-    public RailwayGUI(String title, Graph graph){
+    public RailwayGUI(String title, Graph graph) {
         super(title);
         this.graph = graph;
+
+        initializeGUI();
+        addListeners();
+    }
+
+    private void initializeGUI() {
         mainPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
         fromCity.setModel(new DefaultComboBoxModel<String>(graph.getCities().toArray(new String[0])));
         fromCity.setSelectedIndex(-1);
         toCity.setModel(new DefaultComboBoxModel<String>(graph.getCities().toArray(new String[0])));
         toCity.setSelectedIndex(-1);
 
-        groupButton();
-        initializeGUI();
+        ButtonGroup radioGroup = new ButtonGroup();
+        radioGroup.add(radioTime);
+        radioGroup.add(radioPrice);
+
         this.setContentPane(mainPanel);
     }
 
-    private void groupButton() {
-        ButtonGroup radioGroup = new ButtonGroup();
-
-        radioGroup.add(radioTime);
-        radioGroup.add(radioPrice);
-    }
-
-    private void initializeGUI() {
+    private void addListeners() {
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(fromCity.getSelectedItem() == null || toCity.getSelectedItem() == null){
+                if (fromCity.getSelectedItem() == null || toCity.getSelectedItem() == null) {
                     comboBoxMessageLabel.setText("Choose the departure and arrival point!");
-                }else{
-                    comboBoxMessageLabel.setText("");
-                    if(radioPrice.isSelected()){
-                        radioMessageLabel.setText("");
-                        graph.dijkstra(fromCity.getSelectedItem().toString(), new PriceWeight());
-                        String result = printResult(graph, fromCity.getSelectedItem().toString(), toCity.getSelectedItem().toString());
-                        String route = printPath(graph, fromCity.getSelectedItem().toString(), toCity.getSelectedItem().toString());
-                        infoLabel.setText("The cheapest route: ");
-                        resultLabel.setText(result + " lei");
-                        routeLabel.setText(route);
-                    }else{
-                        if (radioTime.isSelected()) {
-                            radioMessageLabel.setText("");
-                            graph.dijkstra(fromCity.getSelectedItem().toString(), new TimeWeight());
-                            String result = printResult(graph, fromCity.getSelectedItem().toString(), toCity.getSelectedItem().toString());
-                            String route = printPath(graph, fromCity.getSelectedItem().toString(), toCity.getSelectedItem().toString());
-                            infoLabel.setText("The shortest route: ");
-                            resultLabel.setText(result + " min");
-                            routeLabel.setText(route);
-                        }else{
-                            radioMessageLabel.setText("Check in on of these options!");
-                        }
-                    }
+                    return;
+                }
+                comboBoxMessageLabel.setText("");
+                if (radioPrice.isSelected()) {
+                    radioMessageLabel.setText("");
+                    graph.dijkstra(fromCity.getSelectedItem().toString(), new PriceWeight());
+                    String result = printResult(graph, fromCity.getSelectedItem().toString(), toCity.getSelectedItem().toString());
+                    String route = printPath(graph, fromCity.getSelectedItem().toString(), toCity.getSelectedItem().toString());
+                    infoLabel.setText("The cheapest route: ");
+                    resultLabel.setText(result + " lei");
+                    routeLabel.setText(route);
+                } else if (radioTime.isSelected()) {
+                    radioMessageLabel.setText("");
+                    graph.dijkstra(fromCity.getSelectedItem().toString(), new TimeWeight());
+                    String result = printResult(graph, fromCity.getSelectedItem().toString(), toCity.getSelectedItem().toString());
+                    String route = printPath(graph, fromCity.getSelectedItem().toString(), toCity.getSelectedItem().toString());
+                    infoLabel.setText("The shortest route: ");
+                    resultLabel.setText(result + " min");
+                    routeLabel.setText(route);
+                } else {
+                    radioMessageLabel.setText("Check in on of these options!");
                 }
             }
         });
         fromCity.addItemListener(new ItemListener() {
-            String fromItem="";
+            String fromItem = "";
+
             @Override
             public void itemStateChanged(ItemEvent e) {
                 toCity.addItem(fromItem);
                 toCity.removeItem("");
                 fromItem = fromCity.getSelectedItem().toString();
                 toCity.removeItem(fromItem);
-                if(!fromCity.getSelectedItem().toString().equals(fromItem)){
+                if (!fromCity.getSelectedItem().toString().equals(fromItem)) {
                     String newItem = fromCity.getSelectedItem().toString();
                     toCity.addItem(fromItem);
                     toCity.removeItem(newItem);
