36b55ecfac65d31c54c19d3edf40b9dc75da4ea8
Bernadett Farkas <farkas.bernike99@gmail.com>
2020-07-21 20:33:17 +0300

    Line class was added to com.Railway package

---
+++ src/com/Railway/Line.java
@@ -0,0 +1,27 @@
+package com.Railway;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Line extends JPanel {
+    private int x=0;
+    private int y=0;
+    private int z=0;
+    private int t=0;
+
+    public Line(int x, int y, int z, int t) {
+        this.x=x;
+        this.y=y;
+        this.z=z;
+        this.t=t;
+        setSize(500,500);
+    }
+
+    public void paint(Graphics g) {
+
+        //super.paint(g);
+        Graphics2D g2= (Graphics2D) g;
+        g2.setColor(Color.black);
+        g2.drawLine(x,y,z,t);
+    }
+}
